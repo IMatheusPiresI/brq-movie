b256e358d8b715d5bd50951d0e7fabb1be0b6368
Matheus Pires de Sousa <matheuspdsousa@gmail.com>
2023-08-22 10:16:24 -0300

    chore: add theme styled-components and eslint/prettier

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json', '.svg', '.jpg'],
+        alias: {
+          '@src': './src/',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@schemas': './src/schemas',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@routes': './src/routes',
+          '@resources': './src/resources',
+        },
+      },
+    ],
+  ],
 };
